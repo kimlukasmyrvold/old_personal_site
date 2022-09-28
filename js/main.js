0923f4da853be35d22c4794a7be764ac9614433f
kimlukasmyrvold <113918212+kimlukasmyrvold@users.noreply.github.com>
2022-09-28 12:39:59 +0200

    MORE!

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector(".btn-toggle");
+
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme == "dark") {
+    document.body.classList.add("dark-theme");
+    document.getElementById("theme_btn").src = "/img/dark mode button.png";
+}
+
+btn.addEventListener("click", function () {
+    document.body.classList.toggle("dark-theme");
+
+    let theme = "light";
+    if (document.body.classList.contains("dark-theme")) {
+        theme = "dark";
+        document.getElementById("theme_btn").src = "/img/dark mode button.png";
+    }
+    else {
+        document.getElementById("theme_btn").src = "/img/light mode button.png";
+    }
+    localStorage.setItem("theme", theme);
+});
