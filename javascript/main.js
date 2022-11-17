237b3217d843680541bf529da2772dc6152263f4
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2022-11-17 08:37:44 +0100

    Updated the website

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,15 @@
-// Sidemenu script
+// Sidemenu scripts
 
 // Opens and closes sidemenu when clicking on the hamburger icon
 function sidemenu_btn() {
     document.getElementById("sidemenu").classList.toggle("sidemenu-show");
     document.getElementById("sidemenu-overlay").classList.toggle("sidemenu-overlay-show");
+    document.getElementById("appearance-drop").classList.remove("appearance-drop-show");
+    document.getElementById("appearance-btn").classList.remove("appearance-btn-clicked");
+    document.getElementById("language-drop").classList.remove("language-drop-show");
+    document.getElementById("language-btn").classList.remove("language-btn-clicked");
 }
+
 // Opens and closes sidemenu and dropdown menues when clicking outside of sidemenu
 function sidemenu_overlay() {
     document.getElementById("sidemenu-overlay").className = "sidemenu-overlay";
@@ -15,19 +20,66 @@ function sidemenu_overlay() {
     document.getElementById("language-btn").classList.remove("language-btn-clicked");
 }
 
-
-// Dropdown menus scripts
-
-// Appearance dropdown
+// Opens and closes the appearance dropdown menu when is clicked
 function appearance_btn() {
     document.getElementById("appearance-drop").classList.toggle("appearance-drop-show");
     document.getElementById("appearance-btn").classList.toggle("appearance-btn-clicked");
 }
 
-
+// Opens and closes the language dropdown menue when is clicked
 function language_btn() {
     document.getElementById("language-drop").classList.toggle("language-drop-show");
     document.getElementById("language-btn").classList.toggle("language-btn-clicked");
+}
+
+
+
+
+// Changing language of website
+
+// Creating variable for getting the current URL, uses window.location.pathname in order to get what the current URL is.
+// The .pathname is part of the URL after for example: "www.google.com" The pathname is what comes after.
+// Then I add .substring(0, 4) in order to only get the first four letters in the URL
+// The reason why it should only select the 4 first letters is because that is the only information I need in order to check if website is english or not.
+var currentURL = window.location.pathname.substring(0, 4);
+
+// Checks if the varible currentURL is equal to /en/
+// If it is, then it sets the variable lang to english and saves it to localstorage
+// If it is not, then it set the variable lang to norsk and saves it to localstorage
+if (currentURL == "/en/") {
+    var lang = "english";
+    localStorage.setItem("lang", lang);
+}
+else {
+    var lang = "norsk";
+    localStorage.setItem("lang", lang);
+}
+
+// Function for when the english button is pressed.
+// Checks if the variable lang equals norsk
+// if it does, then it changes the pathname of the currenturl and adds /en to the starts of it, thus changing the language of the website to english 
+// Then it lets lang to equal english and saves lang to localstorage
+function language_btn_english() {
+    if (lang == "norsk") {
+        window.location.pathname = "/en" + window.location.pathname;
+        let lang = "english";
+
+        localStorage.setItem("lang", lang);
+    }
+}
+
+// Function for when the norsk button is pressed.
+// Checks if the variable lang equals english
+// if it does, then it changes the pathname of the currenturl and removes /en to the starts of it, thus changing the language of the website to norsk 
+//      Here I used the .slice(3) method in order to remove /en.
+// Then it lets lang to equal norsk and saves lang to localstorage
+function language_btn_norwegian() {
+    if (lang == "english") {
+        window.location.pathname = window.location.pathname.slice(3);
+        let lang = "norsk";
+
+        localStorage.setItem("lang", lang);
+    }
 }
 
 
@@ -37,6 +89,7 @@ function language_btn() {
 
 // Here it reads what the theme currently is, it reads from the localstorage.
 const currentTheme = localStorage.getItem("theme");
+
 // Here it checks if the currentTheme is light or not.
 // If it is equal to light then it adds the light-theme class to the body of the site
 // If it does not equal to light-theme, then it removes the light-theme class
@@ -47,6 +100,7 @@ if (currentTheme == "light") {
 else {
     document.body.classList.remove("light-theme");
 }
+
 // function for when light theme button is pressed
 // when clicked, it will add the "light-theme" class to the body of the site
 // then it lets the variabel "theme" equal "light"
@@ -57,6 +111,7 @@ function theme_btn_light() {
 
     localStorage.setItem("theme", theme);
 }
+
 // function for when dark theme button is pressed
 // when clicked, it removes the "light-theme" class from the body of the site
 // then it lets the variabel "theme" equal "dark"
@@ -67,6 +122,8 @@ function theme_btn_dark() {
 
     localStorage.setItem("theme", theme);
 }
+
+
 
 
 
