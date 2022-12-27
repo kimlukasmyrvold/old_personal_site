3783936bf27f0d024a1fddeaf1579aef58dce8e9
kimlukasmyrvold <kimlukasmryvold06@gmail.com>
2022-12-27 22:14:22 +0100

    Made some changes

--- javascript/main.js
+++ javascript/main.js
@@ -1,99 +1,4 @@
-// Sidemenu scripts
-
-// Opens and closes sidemenu when clicking on the hamburger icon
-function sidemenu_btn() {
-    document.getElementById("sidemenu").classList.toggle("sidemenu");
-    document.getElementById("sidemenu-overlay").classList.toggle("sidemenu-overlay");
-    document.getElementById("appearance-drop").classList.remove("appearance-drop");
-    document.getElementById("appearance-btn").classList.remove("appearance-btn-clicked");
-    document.getElementById("language-drop").classList.remove("language-drop");
-    document.getElementById("language-btn").classList.remove("language-btn-clicked");
-}
-
-// Opens and closes sidemenu and dropdown menues when clicking outside of sidemenu
-function sidemenu_overlay() {
-    document.getElementById("sidemenu-overlay").classList.remove("sidemenu-overlay");
-    document.getElementById("sidemenu").classList.remove("sidemenu");
-    document.getElementById("appearance-drop").classList.remove("appearance-drop");
-    document.getElementById("appearance-btn").classList.remove("appearance-btn-clicked");
-    document.getElementById("language-drop").classList.remove("language-drop");
-    document.getElementById("language-btn").classList.remove("language-btn-clicked");
-}
-
-// Opens and closes the appearance dropdown menu when is clicked
-function appearance_btn() {
-    document.getElementById("appearance-drop").classList.toggle("block");
-    document.getElementById("appearance-btn").classList.toggle("appearance-btn-clicked");
-}
-
-// Opens and closes the language dropdown menue when is clicked
-function language_btn() {
-    document.getElementById("language-drop").classList.toggle("block");
-    document.getElementById("language-btn").classList.toggle("language-btn-clicked");
-}
-
-
-
-
-// Changing language of website
-
-// Creating variable for getting the current URL, uses window.location.pathname in order to get what the current URL is.
-// The .pathname is part of the URL after for example: "www.google.com" The pathname is what comes after.
-// Then I add .substring(0, 7) in order to only get the first seven letters in the URL
-// The reason why it should only select the 7 first letters is because that is the only information the code needs in order to check if website is english or not.
-var currentURL = window.location.pathname.substring(0, 7).toLowerCase();
-
-
-// Checks if the varible currentURL is equal to /cv/en/
-// If it is, then it sets the variable lang to english
-// If it is not, then it set the variable lang to norsk
-if (currentURL === "/cv/en/") {
-    var lang = "english";
-}
-else if (currentURL.substring(0, 4) === "/cv/" && currentURL.substring(4, 7) !== "en/") {
-    var lang = "norsk"
-}
-else if (currentURL.substring(0, 4) === "/en/") {
-    var lang = "english_dev";
-}
-else if (currentURL.substring(0, 4) !== "/cv/" && currentURL.substring(0, 4) !== "/en/") {
-    var lang = "norsk_dev";
-}
-
-// Function for when the english button is pressed.
-// Checks if the variable lang equals norsk
-// if it does, then it changes the pathname of the currenturl and adds /en to the starts of it, thus changing the language of the website to english 
-// Then it lets lang to equal english
-function language_btn_english() {
-    if (lang === "norsk") {
-        window.location.pathname = "/cv/en" + window.location.pathname.replace('/cv', '');
-        let lang = "english";
-    }
-    else if (lang === "norsk_dev") {
-        window.location.pathname = "/en" + window.location.pathname;
-        let lang = "english_dev";
-    }
-}
-
-// Function for when the norsk button is pressed.
-// Checks if the variable lang equals english
-// if it does, then it changes the pathname of the currenturl and removes /en to the starts of it, thus changing the language of the website to norsk 
-// Then it lets lang equal norsk
-function language_btn_norwegian() {
-    if (lang === "english") {
-        window.location.pathname = window.location.pathname.replace('en/', '');
-        let lang = "norsk";
-    }
-    else if (lang === "english_dev") {
-        window.location.pathname = window.location.pathname.slice(3);
-        let lang = "norsk_dev";
-    }
-}
-
-
-
-
-// dark and light theme
+// dark and light theme code
 
 // Here it reads what the theme currently is, it reads from the localstorage.
 const currentTheme = localStorage.getItem("theme");
@@ -130,3 +35,160 @@ function theme_btn_dark() {
 
     localStorage.setItem("theme", theme);
 }
+
+// End of dark / light mode code
+
+
+
+
+
+// Changing language of website
+
+// Const that reads the first 7 letters in the current url of the user. Then makes it all lowercase.
+const currentURL = window.location.pathname.substring(0, 7).toLowerCase();
+
+// Creating the variable lang
+let lang;
+
+// Conditions to check what lang should be, with help from currentURL. If currentURL = /cv/en/ -> lang = english, etc...
+if (currentURL === '/cv/en/') {
+    lang = 'english';
+}
+else if (currentURL.startsWith('/cv/') && !currentURL.endsWith('en/')) {
+    lang = 'norsk';
+}
+else if (currentURL.substring(0, 4) === '/en/') {
+    lang = 'english_dev';
+}
+else {
+    lang = 'norsk_dev';
+}
+
+// Function for when the english button is pressed.
+function language_btn_english() {
+    if (lang === 'norsk') {
+        window.location.pathname = `/cv/en${window.location.pathname.replace('/cv', '')}`;
+        lang = 'english';
+    }
+    else if (lang === 'norsk_dev') {
+        window.location.pathname = `/en${window.location.pathname}`;
+        lang = 'english_dev';
+    }
+}
+
+// Function for when the norsk button is pressed.
+function language_btn_norwegian() {
+    if (lang === 'english') {
+        window.location.pathname = window.location.pathname.replace('en/', '');
+        lang = 'norsk';
+    }
+    else if (lang === 'english_dev') {
+        window.location.pathname = window.location.pathname.slice(3);
+        lang = 'norsk_dev';
+    }
+}
+
+// End of Language code
+
+
+
+
+
+// Sidemenu functions
+
+// Opens and closes sidemenu when clicking on the hamburger icon
+function sidemenu_btn() {
+    document.getElementById("sidemenu").classList.toggle("sidemenu");
+    document.getElementById("sidemenu").classList.toggle("hidden");
+    document.getElementById("sidemenu-overlay").classList.toggle("sidemenu-overlay");
+    document.getElementById("sidemenu-overlay").classList.toggle("hidden");
+    document.getElementById("appearance-drop").classList.remove("block");
+    document.getElementById("appearance-drop").classList.add("hidden");
+    document.getElementById("appearance-btn").classList.remove("appearance-btn-clicked");
+    document.getElementById("language-drop").classList.remove("block");
+    document.getElementById("language-drop").classList.add("hidden");
+    document.getElementById("language-btn").classList.remove("language-btn-clicked");
+}
+
+// Opens and closes sidemenu and dropdown menues when clicking outside of sidemenu
+function sidemenu_overlay() {
+    document.getElementById("sidemenu-overlay").classList.remove("sidemenu-overlay");
+    document.getElementById("sidemenu-overlay").classList.toggle("hidden");
+    document.getElementById("sidemenu").classList.remove("sidemenu");
+    document.getElementById("sidemenu").classList.toggle("hidden");
+    document.getElementById("appearance-drop").classList.remove("block");
+    document.getElementById("appearance-drop").classList.add("hidden");
+    document.getElementById("appearance-btn").classList.remove("appearance-btn-clicked");
+    document.getElementById("language-drop").classList.remove("block");
+    document.getElementById("language-drop").classList.add("hidden");
+    document.getElementById("language-btn").classList.remove("language-btn-clicked");
+}
+
+// Opens and closes the appearance dropdown menu when is clicked
+function appearance_btn() {
+    document.getElementById("appearance-drop").classList.toggle("block");
+    document.getElementById("appearance-drop").classList.toggle("hidden");
+    document.getElementById("appearance-btn").classList.toggle("appearance-btn-clicked");
+}
+
+// Opens and closes the language dropdown menue when is clicked
+function language_btn() {
+    document.getElementById("language-drop").classList.toggle("block");
+    document.getElementById("language-drop").classList.toggle("hidden");
+    document.getElementById("language-btn").classList.toggle("language-btn-clicked");
+}
+
+// End of sidemenu functions
+
+
+
+
+
+// Google maps code
+
+// Function for google maps
+function Map() {
+    // Variable that stores latitude and longitude
+    var location = {
+        lat: 59.260554083311135,
+        lng: 11.107230400640757
+    };
+    // Variable for changing zoom level and where the center of the map view is
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 10,
+        center: location
+    });
+    // Variable for where the marker on the map should be
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+}
+
+
+
+window.onload = function () {
+    const menu_btn = document.querySelector('.hamburger');
+    const appearance_btn = document.querySelector('.appearance');
+    const language_btn = document.querySelector('.language');
+
+    const appearance_drop = document.querySelector('.appearance-drop');
+    const language_drop = document.querySelector('.language-drop');
+
+    menu_btn.addEventListener('click', function () {
+        menu_btn.classList.toggle('is-active');
+    });
+
+    appearance_btn.addEventListener('click', function () {
+        appearance_drop.classList.toggle('hidden');
+        language_drop.classList.add('hidden');
+    })
+
+    language_btn.addEventListener('click', function () {
+        language_drop.classList.toggle('hidden');
+        appearance_drop.classList.add('hidden');
+    })
+
+
+}
+
