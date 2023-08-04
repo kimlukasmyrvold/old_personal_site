3901dba68fc19a8ce3f0dfba56a8c2984efcae6b
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2023-08-04 11:45:35 +0200

    Removed google maps and switched to OpenLayers map

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,8 +60,7 @@ function loadHTML(id, filename, callback) {
     if (!localStorage.getItem('config')) {
         localStorage.setItem('config', JSON.stringify({
             theme: 'device',
-            language: 'no',
-            mapsConsent: 'false'
+            language: 'no'
         }));
     };
     if (!localStorage.getItem('gameData')) {
@@ -243,35 +242,56 @@ class CustomFooter extends HTMLElement {
         this.setAttribute('id', 'footer');
         this.setAttribute('aria-label', 'Primary footer.');
         loadHTML('custom-footer', 'footer.html', () => {
-            // adding google maps script and asking for user consent
-            function addMapsApi() {
-                let config = JSON.parse(localStorage.getItem('config'));
-                config.mapsConsent = true;
-                localStorage.setItem('config', JSON.stringify(config));
-                document.body.appendChild(document.createElement('script')).setAttribute('src', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCriq_RvddTjsYCd-uRVVIJROZpMYBVr0U&callback=Map');
-            }
-            if (JSON.parse(localStorage.getItem('config')).mapsConsent === true) addMapsApi();
-            document.querySelector('#footer #map-consent-button').addEventListener('click', addMapsApi);
+            const openLayersScript = document.createElement('script');
+            openLayersScript.src = 'https://cdn.jsdelivr.net/npm/ol@v7.3.0/dist/ol.js';
+            openLayersScript.onload = function () {
+                const longitude = 11.107192;
+                const latitude = 59.260573;
+
+                let map = new ol.Map({
+                    target: 'map',
+                    layers: [
+                        new ol.layer.Tile({
+                            source: new ol.source.OSM()
+                        })
+                    ],
+                    view: new ol.View({
+                        center: ol.proj.fromLonLat([longitude, latitude]),
+                        zoom: 14
+                    })
+                });
+
+                let marker = new ol.Feature({
+                    geometry: new ol.geom.Point(ol.proj.fromLonLat([longitude, latitude]))
+                });
+
+                let vectorSource = new ol.source.Vector();
+                let vectorLayer = new ol.layer.Vector({
+                    source: vectorSource
+                });
+                vectorSource.addFeature(marker);
+                map.addLayer(vectorLayer);
+
+                let markerStyle = new ol.style.Style({
+                    image: new ol.style.Icon({
+                        anchor: [0.38, 1],
+                        src: '/assets/images/map-marker.svg'
+                    })
+                });
+                marker.setStyle(markerStyle);
+
+                window.addEventListener('resize', function () {
+                    map.updateSize();
+
+                    let extent = map.getView().calculateExtent(map.getSize());
+                    map.getView().fit(extent, { size: map.getSize() });
+                });
+            };
+            document.body.appendChild(openLayersScript);
         });
     };
 };
 customElements.define('custom-footer', CustomFooter);
-
-// Setting location, marker and zoom for google map
-function Map() {
-    const location = {
-        lat: 59.260573,
-        lng: 11.107192
-    };
-    const map = new google.maps.Map(document.querySelector("#footer #map"), {
-        zoom: 12,
-        center: location
-    });
-    new google.maps.Marker({
-        position: location,
-        map: map
-    });
-}
 
 
 
